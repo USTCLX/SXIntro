dedb8623c115f7670ee6b9fdcc3193da9245737b
lixiang <348816986@qq.com>
2018-05-04 11:04:22 +0800

    完成类库初始化

--- src/index.js
+++ src/index.js
@@ -1,34 +1,89 @@
+/**
+ * created by lixiang in 2017/5/2
+ * SXInro 主类
+ */
 
 import utils from './utils';
 
 class SXIntro {
-  constructor(id) {
-    this.targetElement = null;
-    this.introItems = [];
-    this.options = {
-      nextLabel: '下一步 &rarr;',
-      prevLabel: '上一步 &larr;',
-      skipLabel: '跳过',
-      donwLabel: '完成',
-      tooltipPositio: 'bottom',
-      overlayOpacity: 0.8
-    };
-    this.intros = [];
+	constructor(id) {
+		this.targetElement = null;
+		this.introItems = [];
+		this.options = {
+			nextLabel: '下一步 &rarr;',
+			prevLabel: '上一步 &larr;',
+			skipLabel: '跳过',
+			donwLabel: '完成',
+			tooltipPositio: 'bottom',
+			overlayOpacity: 0.8
+		};
+		this.intros = [];
 
-    if (utils.isUndefined(id)) {
-      this.targetElement = document.body;
-    } else if (utils.isString(id)) {
-      this.targetElement = utils.getEleById(id);
-    }
+		this._getTargetElem(id);
 
-    return this;
-  }
+		this._getIntroItems('*[data-step]');
 
-  setIntro(intros) {
-    if (utils.isArray(intros)) {
-      this.intros = intros;
-    }
-  }
+		return this;
+	}
+
+	/**
+   *  设置说明
+   * 数组格式，数组中的次序即代表第几步的引导说明。
+   * @param {arry} intros {1:'第一步：这里是XXX'，2:'第二步:这里是XXX'}
+   */
+	setIntro(intros) {
+		if (utils.isObject(intros)) {
+			this.intros = intros;
+		} else {
+			throw new Error('params must be a object');
+		}
+		const self = this;
+		this.introItems.forEach((item, index) => {
+			item.intro = self.intros[index + 1];
+		});
+
+		return this;
+	}
+
+	/**
+   * 开始启动引导
+   */
+	start() {
+		console.log(this);
+		return this;
+	}
+
+
+	/**
+   * 获取targetElem
+   * @param {string} id
+   */
+	_getTargetElem(id) {
+		if (utils.isUndefined(id)) {
+			this.targetElement = document.body;
+		} else if (utils.isString(id)) {
+			this.targetElement = utils.getEleById(id);
+		}
+	}
+
+	/**
+	 * 获取需要引导的元素集合
+	 * @param {String} selector
+	 */
+	_getIntroItems(selector) {
+		const tempIntroItems = utils.querySelector(this.targetElement, selector);
+		let step = 0;
+		this.introItems = [];
+		for (let i = 0, il = tempIntroItems.length; i < il; i++) {
+			step = parseInt(utils.getAttribute(tempIntroItems[i], 'data-step'), 10);
+			this.introItems.push({
+				elem: tempIntroItems[i],
+				intro: '',
+				step
+			});
+		}
+		this.introItems.sort((a, b) => a.step - b.step);
+	}
 }
 
 export default SXIntro;
--- src/utils.js
+++ src/utils.js
@@ -5,62 +5,52 @@
 
 
 const BaseType = {
-  String: 'string',
-  Object: 'object',
-  Function: 'function',
-  Boolean: 'boolean',
-  Array: 'array',
-  RegExp: 'regexp',
-  Number: 'number'
+	String: 'string',
+	Object: 'object',
+	Function: 'function',
+	Boolean: 'boolean',
+	Array: 'array',
+	RegExp: 'regexp',
+	Number: 'number'
 };
 
 /**
  * 返回对象类型，小写字符串
  */
 function checkType(obj) {
-  const str = Object.prototype.toString.call(obj);
-  return str.slice(8, str.length - 1).toLowerCase();
+	const str = Object.prototype.toString.call(obj);
+	return str.slice(8, str.length - 1).toLowerCase();
 }
 
 /**
 * 深拷贝
 */
 function deepClone(source) {
-  let copy;
-  if (source === null || typeof values !== 'object') {
-    return source;
-  }
+	let copy;
+	if (source === null || typeof values !== 'object') {
+		return source;
+	}
 
-  if (source instanceof Date) {
-    copy = new Date();
-    copy.setTime(source.getTime());
-  }
+	if (source instanceof Date) {
+		copy = new Date();
+		copy.setTime(source.getTime());
+	}
 
-  if (source instanceof Array) {
-    copy = [];
-    for (let i = 0, il = source.length; i < il; ++i) {
-      copy[i] = deepClone(source[i]);
-    }
-  }
+	if (source instanceof Array) {
+		copy = [];
+		for (let i = 0, il = source.length; i < il; ++i) {
+			copy[i] = deepClone(source[i]);
+		}
+	}
 
-  if (source instanceof Object) {
-    copy = {};
-    const arr = Object.keys(source);
-    arr.forEach((key) => {
-      copy[key] = deepClone(source[key]);
-    });
-  }
-  return copy;
-}
-
-function getEleById(id) {
-  let ele;
-  if (checkType(id) === BaseType.String) {
-    ele = document.getElementById(id);
-  } else {
-    ele = null;
-  }
-  return ele;
+	if (source instanceof Object) {
+		copy = {};
+		const arr = Object.keys(source);
+		arr.forEach((key) => {
+			copy[key] = deepClone(source[key]);
+		});
+	}
+	return copy;
 }
 
 /**
@@ -69,7 +59,7 @@ function getEleById(id) {
  * @returns {boolean}
  */
 function isString(obj) {
-  return checkType(obj) === BaseType.String;
+	return checkType(obj) === BaseType.String;
 }
 
 /**
@@ -78,7 +68,7 @@ function isString(obj) {
 * @returns {boolean}
 */
 function isNumber(obj) {
-  return checkType(obj) === BaseType.Number;
+	return checkType(obj) === BaseType.Number;
 }
 
 /**
@@ -87,29 +77,64 @@ function isNumber(obj) {
 * @returns {boolean}
 */
 function isObject(obj) {
-  return checkType(obj) === BaseType.Object;
+	return checkType(obj) === BaseType.Object;
 }
 
+/**
+ * 判断对象是否为undefined
+ * @param {object} obj
+ */
 function isUndefined(obj) {
-  return obj === undefined;
+	return obj === undefined;
 }
 
+/**
+ * 判断对象是否为数组
+ * @param {object} obj
+ */
 function isArray(obj) {
-  return checkType(obj) === BaseType.Array;
+	return checkType(obj) === BaseType.Array;
 }
 
+/**
+ * 根据id获取Dom元素
+ * @param {string} id
+ */
+function getEleById(id) {
+	let ele;
+	if (checkType(id) === BaseType.String) {
+		ele = document.getElementById(id);
+	} else {
+		ele = null;
+	}
+	return ele;
+}
 
+/**
+ * 获取元素集合
+ * @param {DOM} baseElement
+ * @param {选择器} selectors
+ */
 function querySelector(baseElement, selectors) {
-  let eleList = [];
-
-  if (!isObject(baseElement) || !isString(selectors) || !baseElement.querySelectorAll) {
-    throw new Error('querySelector err');
-  }
-
-  eleList = baseElement.querySelectorAll(selectors);
-  return eleList;
+	if (!baseElement || !isString(selectors) || !baseElement.querySelectorAll) {
+		throw new Error('querySelector err');
+	}
+	return baseElement.querySelectorAll(selectors);
 }
+
+function getAttribute(elem, attr) {
+	return elem.getAttribute(attr);
+}
+
 export default {
-  deepClone, getEleById, isNumber, isObject, isString, isUndefined, isArray, querySelector
+	deepClone,
+	getEleById,
+	isNumber,
+	isObject,
+	isString,
+	isUndefined,
+	isArray,
+	querySelector,
+	getAttribute
 };
 
